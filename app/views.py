bd8dd746862d400b0245bbd9e7de842c445ab4a5
CeciliaIda Watt <ciw2104@columbia.edu>
2015-11-15 09:29:08 -0500

    adding styles

--- app/views.py
+++ app/views.py
@@ -9,3 +9,11 @@ def index():
 @app.route('/chart')
 def chart():
 	return render_template('chart.html', title='hey')
+
+@app.route('/map1')
+def test():
+	return render_template('map1.html')
+
+@app.route('/time1')
+def time1():
+	return render_template('time1.html')
